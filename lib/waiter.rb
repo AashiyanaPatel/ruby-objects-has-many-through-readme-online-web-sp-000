67fd065fd8c201a58027922206fd3133de023bae
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-03-08 21:54:11 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -25,9 +25,9 @@ class Waiter
   end
   
   def best_tipper
-    best_tipper_meal = meals.max do |meal_a, meal_b|
+    best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
     end
-    best_tipper_meal
+    best_tipped_meal
   end
 end
